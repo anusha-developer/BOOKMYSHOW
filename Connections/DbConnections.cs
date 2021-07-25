cf2aa44215a55f81fb3a4a99ab330a35f2db02c8
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-07-25 11:00:20 +0530

    Add files via upload

---
+++ Connections/DbConnections.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace BOOKMYSHOW.Connections
+{
+    public class DbConnections
+    {
+        public static SqlConnection CreateConnection()
+        {
+            SqlConnection con = new SqlConnection("data source=.; database=BOOk_MY_SHOW; integrated security=SSPI");
+            return con;
+        }
+    }
+}
